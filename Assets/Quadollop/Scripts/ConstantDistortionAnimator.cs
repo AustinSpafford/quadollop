4a60d154e3bd4909bddd4bf5e51954c1f4b3530e
Austin Spafford <austin.spafford@gmail.com>
2016-05-31 17:52:29 -0700

    Adding a constant-speed rotation on the distortion material

---
+++ Assets/Quadollop/Scripts/ConstantDistortionAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantDistortionAnimator : DistortionAnimatorBase
+{
+	public float CycleSeconds = 2.0f;
+
+	public override float GetCurrentDistortionDegrees()
+	{
+		float animationFraction = 
+			Mathf.Repeat(
+				(Time.time / CycleSeconds),
+				1.0f);
+
+		return (360.0f * animationFraction);
+	}
+}
---
+++ Assets/Quadollop/Scripts/DistortionAnimatorBase.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class DistortionAnimatorBase : MonoBehaviour
+{
+	public void Awake()
+	{
+		materialInstance = GetComponent<Renderer>().material;
+
+		distortionDegreesPropertyId = Shader.PropertyToID("_DistortionDegrees");
+
+		if (materialInstance.HasProperty(distortionDegreesPropertyId) == false)
+		{
+			throw new System.InvalidOperationException();
+		}
+	}
+
+	public void Update()
+	{
+		if (materialInstance != null)
+		{
+			materialInstance.SetFloat(
+				distortionDegreesPropertyId,
+				GetCurrentDistortionDegrees());
+		}
+	}
+
+	public void OnDestroy()
+	{
+		Destroy(materialInstance);
+	}
+
+	public abstract float GetCurrentDistortionDegrees();
+
+	private Material materialInstance = null;
+	private int distortionDegreesPropertyId = -1;
+}
